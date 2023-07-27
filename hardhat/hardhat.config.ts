93724733206ae9ae154e2ea96c52434b0bc0d9d0
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-07-27 23:11:02 +0530

    hardhat_included

---
+++ hardhat/hardhat.config.ts
@@ -0,0 +1,17 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "local",
+  solidity: "0.8.19",
+  networks: {
+    local: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+  paths: {
+    artifacts: "../src",
+  },
+};
+
+export default config;
---
+++ hardhat/scripts/deploy.ts
@@ -0,0 +1,33 @@
+import { writeFile } from "fs";
+import { ethers } from "hardhat";
+
+async function main() {
+  const cert = await ethers.deployContract("Cert");
+
+  await cert.waitForDeployment();
+
+  const details = {
+    deployer: cert.deploymentTransaction()?.from,
+    contract: await cert.getAddress(),
+  };
+  
+  console.log(`${details.deployer} deployed ${details.contract}`);
+
+  writeFile(
+    "../src/details.json",
+    JSON.stringify(details, null, 2),
+    (err) => {
+      if (err) {
+        return console.log(err);
+      }
+      return console.log("Details are saved!!");
+    }
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
