ae747be5d183b573fc0a78b51e64a7bf67025469
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-09-26 20:23:57 +0530

    💥 | changed from 'node' to 'bun'

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -3,7 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "local",
-  solidity: "0.8.19",
+  solidity: "0.8.20",
   networks: {
     local: {
       url: "http://127.0.0.1:8545",
--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,28 +1,23 @@
-import { writeFile } from "fs";
 import { ethers } from "hardhat";
+import { writeFile } from "node:fs";
 
 async function main() {
   const cert = await ethers.deployContract("Cert");
-
   await cert.waitForDeployment();
 
   const details = {
     deployer: cert.deploymentTransaction()?.from,
     contract: await cert.getAddress(),
   };
-  
+
   console.log(`${details.deployer} deployed ${details.contract}`);
 
-  writeFile(
-    "../src/details.json",
-    JSON.stringify(details, null, 2),
-    (err) => {
-      if (err) {
-        return console.log(err);
-      }
-      return console.log("Details are saved!!");
+  writeFile("../src/details.json", JSON.stringify(details, null, 2), (err) => {
+    if (err) {
+      return console.log(err);
     }
-  );
+    return console.log("Details are saved!!");
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
